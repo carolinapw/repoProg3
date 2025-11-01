4170e0c8bb2066a3242c2de1c7dbffde0cfa27cd
carolinapw <cpicowertag@udesa.edu.ar>
2025-11-01 19:56:56 -0300

    css screen users

--- clase10/ejercicio/src/screens/Users.js
+++ clase10/ejercicio/src/screens/Users.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
 import { db } from "../firebase/config";
 
-export default class Users extends Component {
+class Users extends Component {
   constructor(props) {
     super(props);
     this.state = { users: [], loading: true };
@@ -23,14 +23,14 @@ export default class Users extends Component {
   render() {
     return (
       <View>
-        <Text>Usuarios</Text>
+        <Text style={styles.title}>Usuarios</Text>
         <FlatList
             data={this.state.users}
             keyExtractor={item => item.id.toString()}
             renderItem={ ({item}) => 
-                <View>
-                    <Text>{item.data.email}</Text>
-                    <Text>{item.data.userName}</Text>
+                <View style={styles.row}>
+                    <Text style={styles.email}>Email: {item.data.email}</Text>
+                    <Text style={styles.user}>Usuario: {item.data.userName}</Text>
                 </View>
             }
         />
@@ -39,3 +39,20 @@ export default class Users extends Component {
   }
 }
 
+const styles = StyleSheet.create({
+  page: { flex: 1, padding: 16, backgroundColor: "#fff" },
+  title: { fontSize: 30, fontWeight: "800", padding: 22 },
+  row: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "#e6e6e6",
+    marginHorizontal: 22, 
+    marginVertical: 2,  
+  },
+  email: { fontWeight: "700" },
+  user: { color: "#333", marginTop: 2 },
+});
+
+export default Users
